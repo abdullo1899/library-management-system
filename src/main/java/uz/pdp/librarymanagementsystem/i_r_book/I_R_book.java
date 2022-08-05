6877d1f094607d040c932a18eae1266d61f3ddb3
abdullo1899 <abdullo06072006@gmail.com>
2022-08-05 16:59:06 +0500

    getImgUrl() method added

--- src/main/java/uz/pdp/librarymanagementsystem/i_r_book/I_R_book.java
+++ src/main/java/uz/pdp/librarymanagementsystem/i_r_book/I_R_book.java
@@ -17,10 +17,7 @@ public class I_R_book {
     private Long id;
     private Date date;
     private Boolean is_issued;
-
     private Long student_id;
-
     private Set<Book> book_ids;
-
     private Set<Long> books;
 }
--- src/main/java/uz/pdp/librarymanagementsystem/utils/Util.java
+++ src/main/java/uz/pdp/librarymanagementsystem/utils/Util.java
@@ -4,4 +4,8 @@ public class Util {
 
     public final static String UPLOAD_DIRECTORY = "C:\\Users\\User\\Desktop\\images";
 //    public final static String UPLOAD_DIRECTORY = "C:/Users/User/Desktop/images";
+
+    public static String getImageUrl(String text){
+        return text;
+    }
 }
